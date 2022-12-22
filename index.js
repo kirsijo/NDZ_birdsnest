751221f2f788394d37baa439634d3632dae3d4a7
Kirsi Jokela <kirsijo@gmail.com>
2022-12-22 16:06:17 +0200

    parsing xml to json, returning drone data

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const axios = require('axios');
+const xml2js = require('xml2js').parseString;
+
+app.get('/', async (request, response) => {
+    const data = await axios.get('https://assignments.reaktor.com/birdnest/drones')
+    const parsedData = xml2js(data.data);
+    console.log(parsedData);
+    response.send(parsedData);
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
