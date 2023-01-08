fd871dc7096f700cbe822b727b1129d0ed5af930
Kirsi Jokela <kirsijo@gmail.com>
2023-01-08 18:14:17 +0200

    axios, drone card component and app set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
 import "./App.css";
+import DroneCard from "./components/DroneCard";
+import axios from "axios";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <h1>App will be here</h1>
+      <div className="header">
+        <header>NDZ - Operation Bird's Nest</header>
+      </div>
+      <p>Drone Sightings</p>
+      <div className="drone-container"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/DroneCard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DroneCard = () => {
+    <div>Drone card will be here</div>
+}
+
+export default DroneCard;
